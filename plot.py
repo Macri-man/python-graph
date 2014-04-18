2c7f51b471f730f231f6eea1a45b21e199021e61
Alexander D. Macri <admfav2012@gmail.com>
2014-04-18 17:49:13 +0000

    Update plot.py

--- plot.py
+++ plot.py
@@ -2,7 +2,21 @@ import pylab
 
 #list_of_files=[ ('binarysearchoutput.txt', 'Binary Search Insertion Sort'), ('heapoutput.txt', 'Heap Sort'), ('mergeoutput.txt', 'Merge Sort'), ('insertionoutput.txt', 'Insertionsort'),('selectionoutput.txt', 'Selection Sort'), ('quickoutput.txt', 'Quick Sort')]
 
-list_of_files=[ ('selectionoutput.txt',' QuickSort Selection'),('medianoutput.txt','Median of Medians')]
+#list_of_files=[ ('selectionoutput.txt',' QuickSort Selection'),('medianoutput.txt','Median of Medians')]
+
+#list_of_files=[ ('binarysearchoutput.txt','Binary Search Insertion Sort'),('theorybinarysearchoutput.txt', 'Theoretical Values')]
+
+#list_of_files=[ ('heapoutput.txt','Heap Sort'),('theoryheapoutput.txt', 'Theoretical Values')]
+
+#list_of_files=[ ('mergeoutput.txt', 'Merge Sort'),('theorymergeoutput.txt', 'Theoretical Values')]
+
+#list_of_files=[ ('insertionoutput.txt', 'Insertion Sort'),('theoryinsertionoutput.txt', 'Theoretical Values')]
+
+#list_of_files=[ ('selectionoutput.txt', 'Selection Sort'),('theoryselectionoutput.txt', 'Theoretical Values')]
+
+#list_of_files=[ ('quickoutput.txt', 'Quick Sort'),('theoryquickoutput.txt', 'Theoretical Values')]
+
+#list_of_files=[ ('randquickoutput.txt','Randomized Quick Sort'),('theoryrandquickoutput.txt', 'Theoretical Values')]
 
 datalist = [ ( pylab.loadtxt(filename), label ) for filename, label in list_of_files ]
 pylab.figure()
